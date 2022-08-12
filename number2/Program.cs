0e14f80515ab7416542cce8a602185396640b30e
Ilya <klind2005@gmail.com>
2022-08-12 19:01:40 +0300

    Вторая

---
+++ number2/Program.cs
@@ -0,0 +1,36 @@
+﻿int a, b, c;
+int mmax;
+
+Console.WriteLine("Введите три числа:");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    mmax = a;
+}
+else
+{
+    mmax = b;
+}
+
+if (b > c)
+{
+    mmax = b;
+}
+else
+{
+    mmax = c;
+}
+
+if (a > c)
+{
+    mmax = a;
+}
+else
+{
+    mmax = c;
+}
+
+Console.WriteLine(mmax);
