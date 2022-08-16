145a832a0fefaf510630702f7700aa96d17d9722
Ilya <klind2005@gmail.com>
2022-08-16 14:00:12 +0300

    n2.3

---
+++ number2.3/Program.cs
@@ -0,0 +1,17 @@
+﻿int a;
+
+Console.WriteLine("Введите номер дня недели:");
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 6)
+{
+    Console.WriteLine(a + " Это рабочий день");
+}
+else if (a == 6)
+{
+    Console.WriteLine(a + " Это суббота");
+}
+else if (a == 7)
+{
+    Console.WriteLine(a + " Это воскресенье");
+}
