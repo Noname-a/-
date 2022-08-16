7e1e6e0ae469e4bcfd980ed9e1cfb25f72405952
Ilya <klind2005@gmail.com>
2022-08-16 13:47:33 +0300

    n2.2

---
+++ number2.2/Program.cs
@@ -0,0 +1,14 @@
+﻿int a;
+
+Console.WriteLine("Введите число:");
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 99)
+{
+    int b = (a % 100) % 10;
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
