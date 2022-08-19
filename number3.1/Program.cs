eba68210d4776bfa8bec8464f1ca3771b3934278
Ilya <klind2005@gmail.com>
2022-08-19 15:30:02 +0300

    3.1

---
+++ number3.1/Program.cs
@@ -0,0 +1,22 @@
+﻿int num, rem, sum = 0, temp = 0;
+
+Console.WriteLine("Введите пятизначное число:");
+num = Convert.ToInt32(Console.ReadLine());
+temp = num;
+
+while (num > 0)
+{
+    rem = num % 10; 
+    num = num / 10; 
+    sum = sum * 10 + rem;
+}
+Console.WriteLine("Перевернутое число: " + sum);
+
+if (temp == sum)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else 
+{
+    Console.WriteLine("Число не является палиндромом");
+}
