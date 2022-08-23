bc15e9950d447739d3e6909a819654b93fb7e39e
Ilya <klind2005@gmail.com>
2022-08-23 09:09:53 +0300

    4.2

---
+++ number4.2/Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+int sum = 0;
+
+Console.WriteLine("Введите число:");
+a = Convert.ToInt32(Console.ReadLine());
+
+while (a > 0)
+{
+    sum = sum + a % 10;
+    a /= 10;
+}
+
+Console.WriteLine("Сумма чисел: " + sum);
