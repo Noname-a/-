d07d69e6b2a6fcee794ab3bacf795659cf4e8390
Ilya <klind2005@gmail.com>
2022-08-23 08:52:16 +0300

    4.1

---
+++ number4.1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+int AB = a;
+
+for (int i = 1; i < b; i++)
+	{
+		AB *= a;
+	}
+Console.WriteLine("Число A в степени B: " + AB);
