baa07e3371ce84656b863821d28bf9009c97af3d
Ilya <klind2005@gmail.com>
2022-08-26 19:06:12 +0300

    5.2

---
+++ number5.2/Program.cs
@@ -0,0 +1,12 @@
+﻿var r = new Random();
+int[] a = new int[4];
+int sum = 0;
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = r.Next(0,11);
+    Console.WriteLine(a[i]);
+    if (i % 2 == 1)
+        sum += a[i];
+}
+
+Console.Write("Сумма нечетных чисел: "+sum);
