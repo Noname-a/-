c7022274555779042ec640b444e67512e184aae7
Ilya <klind2005@gmail.com>
2022-08-26 19:52:21 +0300

    5.3

---
+++ number5.3/Program.cs
@@ -0,0 +1,16 @@
+﻿var r = new Random();
+int[] a = new int[4];
+int imin = 0;
+int imax = 0;
+
+for (int i = 1; i < a.Length; i++)
+{
+    a[i] = r.Next(0,100);
+    Console.WriteLine(a[i]);
+    if (a[i] < a[imin]) 
+    imin = i;
+    if (a[i] > a[imax]) 
+    imax = i;
+}
+int b = imax - imin;
+Console.Write("Разница между максимальным и минимальным: " + b);
