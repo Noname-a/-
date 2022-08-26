b202c8a7ba21fe8da5ae6bbb7538e73996289fe5
Ilya <klind2005@gmail.com>
2022-08-26 18:39:39 +0300

    5.1

---
+++ number5.1/Program.cs
@@ -0,0 +1,11 @@
+﻿var r = new Random();
+int[] array = new int[3];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = r.Next(99,1000);
+    Console.WriteLine(array[i]);
+    if ((array[i] % 2) == 0)
+        count++;
+}
+Console.Write("Количество четных чисел в массиве: "+count);
