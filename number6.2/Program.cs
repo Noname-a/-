c60c2bae161442d61353e5c39e2c8e168d2150c8
Ilya <klind2005@gmail.com>
2022-08-29 21:48:10 +0300

    6.2

---
+++ number6.2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите k1: ");
+int k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите b1: ");
+int b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+int k2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+int b2 = int.Parse(Console.ReadLine());
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
