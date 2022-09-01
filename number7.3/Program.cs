bd43ea92eca46bfeeab6ac46397cf97b2459a98b
Ilya <klind2005@gmail.com>
2022-09-01 20:38:51 +0300

    7.3

---
+++ number7.3/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = 4; 
+int m = 4; 
+var r = new Random();
+int[,] arr = new int[n, m];
+Console.WriteLine("Массив:");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = r.Next(0, 99);
+        Console.Write("{0} ", arr[i, j]);
+    }
+    Console.WriteLine();
+}
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ sum / arr.GetLength(0)} ");
+}
