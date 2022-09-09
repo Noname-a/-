a396ccfa23249935268ad467505242d26256b360
Ilya <klind2005@gmail.com>
2022-09-09 03:43:29 +0300

    9.1

---
+++ number9.1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число больше 0: ");
+int i = int.Parse(Console.ReadLine());
+
+void RecursionNumbers(int i)
+{
+    if (i == 1)
+    {
+        Console.Write($" {i}");
+    }
+    else
+    {
+        Console.Write($" {i}");
+        i--;
+        RecursionNumbers(i);
+    }
+}
+
+RecursionNumbers(i);
