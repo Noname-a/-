e9c9d9637d454c5e90ace9e2dfa5590cc0e0d421
Ilya <klind2005@gmail.com>
2022-09-09 04:03:16 +0300

    9.3

---
+++ number9.3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine());
+static int A(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+}
+A( n,  m);
+Console.WriteLine($"Итог: {A(n,m)}");
+    
