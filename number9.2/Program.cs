d7e0b16734b72a05ef031a13a761bf1f7a2c6f91
Ilya <klind2005@gmail.com>
2022-09-09 03:44:15 +0300

    9.2

---
+++ number9.2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите b: ");
+int b = int.Parse(Console.ReadLine());
+
+static int RecursiveMethod(int a, int b)
+{
+    Console.WriteLine(a);
+    if (a == b)
+    {
+        return a;
+    }
+    else 
+    {
+        return a + RecursiveMethod(a + 1, b); 
+    }
+}
+Console.WriteLine($"Сумма = {RecursiveMethod(a, b)}");
+
